64b5cd6afbb58b84509e60c33a8eedc16481bb12
Leonel Valencia <valenciaestudillo@gmail.com>
2024-02-20 06:44:50 -0600

    add FrasesParaPonerEnTextoaRegulonDB.csv

--- make_phrases.py
+++ make_phrases.py
@@ -89,11 +89,15 @@ if r.status_code == 200:
         for operon in r.json()["data"]["getAllOperon"]["data"]:
             for tu in operon["transcriptionUnits"]:
                 if tu["promoter"]:
+                  # 1. The name of the promoter with the identifier of RegulonBB database [PROMOTER_ID] is  [PROMOTER_NAME]
                     phrases.append(f"The name of the promoter with the identifier of RegulonBB database {tu['promoter']['_id']} is {tu['promoter']['name']}")
                     for citation in tu["promoter"]["citations"]:
                         if citation["evidence"]:
+                            #3. The promoter  [PROMOTER_NAME] of  Escherichia coli K-12 has the evidence ["EVIDENCE"] linked to the reference [PMID:REFERENCE_ID]
                             phrases.append(f"The promoter {tu['promoter']['name']} of  Escherichia coli K-12 has the evidence '{citation['evidence']['name']}' linked to the reference {citation['publication']['pmid']}")
+                        # 4. The promoter [PROMOTER_NAME] of  Escherichia coli K-12 has the reference [REFERENCE_ID]
                         phrases.append(f"The promoter {tu['promoter']['name']} of  Escherichia coli K-12 has the reference {citation['publication']['pmid']}")
+                    # 5. The Transcription Start Site (TSS) of the promoter [PROMOTER_NAME] of  Escherichia coli K-12 is located at genome position [POS_1]
                     phrases.append(f"The Transcription Start Site (TSS) of the promoter {tu['promoter']['name']} of  Escherichia coli K-12 is located at genome position {tu['promoter']['transcriptionStartSite']['leftEndPosition']}")    
     print(phrases)
 else:
