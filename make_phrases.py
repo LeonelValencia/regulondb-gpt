32e2b87df3b9d65c15576ac5e1858dc20b5d9738
Leonel Valencia <valenciaestudillo@gmail.com>
2024-02-20 23:30:35 -0600

    complete variables of df unless 10 and 11

--- make_phrases.py
+++ make_phrases.py
@@ -1,6 +1,7 @@
-
 import requests
 import json
+import re
+import pandas as pd
 
 endpoint = "https://regulondb.ccg.unam.mx/graphql"
 
@@ -145,6 +146,11 @@ if r.status_code == 200:
                         if citation["evidence"]:
                             #3. The promoter  [PROMOTER_NAME] of  Escherichia coli K-12 has the evidence ["EVIDENCE"] linked to the reference [PMID:REFERENCE_ID]
                             phrases.append(f"The promoter {tu['promoter']['name']} of Escherichia coli K-12 has the evidence '{citation['evidence']['name']}' linked to the reference {citation['publication']['pmid']}")
+                            # 14. The -10 and -35 boxes of the [PROMOTER_NAME] of  Escherichia coli K-12 has the evidence ["EVIDENCE"]
+                            phrases.append(f"The -10 and -35 boxes of the {tu['promoter']['name']} of Escherichia coli K-12 has the evidence '{citation['evidence']['name']}'")
+                            if tu["promoter"]["bindsSigmaFactor"]["name"]:
+                                # 16. The [Sigma_factor] transcribing the [PROMOTER_NAME] of  Escherichia coli K-12 has the evidence related to the promoter ["EVIDENCE"]
+                                phrases.append(f"The {tu['promoter']['bindsSigmaFactor']['name']} transcribing the {tu['promoter']['name']} of Escherichia coli K-12 has the evidence related to the promoter '{citation['evidence']['name']}'")
                         # 4. The promoter [PROMOTER_NAME] of  Escherichia coli K-12 has the reference [REFERENCE_ID]
                         phrases.append(f"The promoter {tu['promoter']['name']} of Escherichia coli K-12 has the reference {citation['publication']['pmid']}")
                     # 5. The Transcription Start Site (TSS) of the promoter [PROMOTER_NAME] of  Escherichia coli K-12 is located at genome position [POS_1]
@@ -154,13 +160,14 @@ if r.status_code == 200:
                     phrases.append(f"The promoter {tu['promoter']['name']} of Escherichia coli K-12 has a TSS located at {strand}{tu['firstGene']['distanceToPromoter']} relative position from the gene {tu['firstGene']['name']}")
                     # 7. The promoter [PROMOTER_NAME] of Escherichia coli K-12 has a TSS at a [A,T,G,C] nucleotide
                     sequence = tu['promoter']['sequence']
-                    uppercase_letters = [char for char in sequence if char.isupper()]
+                    uppercase_letters = re.findall(r'[A|T|G|C]', sequence)
                     phrases.append(f"The promoter {tu['promoter']['name']} of Escherichia coli K-12 has a TSS at a {uppercase_letters[0]} nucleotide")
                     if tu["promoter"]["boxes"]:
                       # 8. The -10 box of the [PROMOTER_NAME] of  Escherichia coli K-12 is located at genome positions [star position in the genome of the -10 box] and [end position in the genome of the -10 box]
                       phrases.append(f"The -10 box of the {tu['promoter']['name']} of Escherichia coli K-12 is located at genome positions {tu['promoter']['boxes'][0]['rightEndPosition']} and {tu['promoter']['boxes'][0]['leftEndPosition']}")    
                       # 9. The -35 box of the [PROMOTER_NAME] promoter of  Escherichia coli K-12 is located at genome positions [star position in the genome of the -10 box] and [end position in the genome of the -35 box]
                       phrases.append(f"The -35 box of the {tu['promoter']['name']} promoter of Escherichia coli K-12 is located at genome positions {tu['promoter']['boxes'][1]['rightEndPosition']} and {tu['promoter']['boxes'][1]['leftEndPosition']}")
+                      # 10. The -10 box of the [PROMOTER_NAME] promoter of Escherichia coli K-12 is located at [relative position of the end of the -10 box regarding to the TSS] position relative to the TSS 
                       # 12. The sequence of the -10 box of the promoter [PROMOTER_NAME] of  Escherichia coli K-12 is [sequence of the -10 box of the [PROMOTER_NAME] ]
                       phrases.append(f"The sequence of the -10 box of the promoter {tu['promoter']['name']} of Escherichia coli K-12 is {tu['promoter']['boxes'][0]['sequence']}")
                       # 13. The sequence of the -35 box of the promoter [PROMOTER_NAME] of  Escherichia coli K-12 is [sequence of the -35 box of the [PROMOTER_NAME] ]
@@ -171,7 +178,9 @@ if r.status_code == 200:
                     # 17. The sequence of the promoter [PROMOTER_NAME] of  Escherichia coli K-12  is [sequence of the promoter]
                     phrases.append(f"The sequence of the promoter {tu['promoter']['name']} of Escherichia coli K-12 is {tu['promoter']['sequence']}")
                     # 18. The transcription of the transcription unit (TU) [TU_name] is started at the promoter  [PROMOTER_NAME] of  Escherichia coli K-12
-                    # phrases.append(f"The transcription of the transcription unit (TU) {tu['name']} is started at the promoter {tu['promoter']['name']} of Escherichia coli K-12")
+                    phrases.append(f"The transcription of the transcription unit (TU) {tu['name']} is started at the promoter {tu['promoter']['name']} of Escherichia coli K-12")
+                    # 19. The transcription of the gene  [gene_name] is started at the promoter [PROMOTER_NAME] of Escherichia coli K-12
+                    phrases.append(f"The transcription of the gene {tu['firstGene']['name']} is started at the promoter {tu['promoter']['name']} of Escherichia coli K-12")
                     if tu["promoter"]["regulatorBindingSites"]:
                       for regulatorBindingS in tu["promoter"]["regulatorBindingSites"]:
                         for regulatoryInteraction in regulatorBindingS["regulatoryInteractions"]:
@@ -188,8 +197,8 @@ if r.status_code == 200:
                       # 22. "The comment related to the [PROMOTER_NAME] promoter of  Escherichia coli K-12 is ""[PROMOTER_NOTE] """
                       phrases.append(f"The comment related to the {tu['promoter']['name']} promoter of Escherichia coli K-12 is '{tu['promoter']['note']}'")
               
-    
-    print(phrases)
+    df = pd.DataFrame(phrases, columns=["Phrases"])
+    print(df)
 else:
     raise Exception(f"Query failed to run with a {r.status_code}.")
     
